3deea380cffecebebe9bb26db982ae756d6c1752
Patrick Thompson <patrick.thompson@dfo-mpo.gc.ca>
2024-05-21 20:35:24 -0700

    adding pt theme function and documentation

---
+++ R/pt_theme.R
@@ -0,0 +1,20 @@
+#' A custom ggplot theme
+#'
+#' This function applies a custom theme to a ggplot object. It is based on
+#' theme_bw(), but with grid lines removed and facet boxes white with no border.
+#'
+#' @examples
+#' # Create a basic ggplot
+#' p <- ggplot(mtcars, aes(mpg, hp)) + geom_point()
+#'
+#' # Apply the custom theme
+#' p + theme_custom()
+
+theme_pt <- function() {
+  theme_bw() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      strip.background = element_rect(fill = "white", colour = NA)
+    )
+}
