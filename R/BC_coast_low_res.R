9da6371b4459dd1d23ea1122a8a78f93dc96a888
Patrick Thompson <patrick.thompson@dfo-mpo.gc.ca>
2024-05-21 21:08:35 -0700

    adding BC coastline polygon file

---
+++ R/BC_coast_low_res.R
@@ -0,0 +1,9 @@
+#' BC Coast Low Res
+#'
+#' A low resolution sf polygon shape file for the BC coastline.
+#'This is an appropriate resolution for coastwide maps but does not have sufficient resolution for maps of small areas.
+#'
+#' @format An object of class \code{sf} (simple feature) with 783 features and 8 fields.
+#' The projected CRS is NAD83 / BC Albers (EPSG:3005).
+#' @source Obtained from the Canadian Hydrographic Service.
+"BC_coast_low_res"
