1e72a0b5a25e3a2d2d7d29eee39294040846b911
Patrick Thompson <patrick.thompson@dfo-mpo.gc.ca>
2024-05-21 21:15:47 -0700

    add import of GeomViolin

--- R/geom_split_violin.R
+++ R/geom_split_violin.R
@@ -6,6 +6,7 @@
 #' @param ... Other arguments passed on to methods.
 #' @return A ggplot grob.
 #' @seealso \code{\link[ggplot2]{GeomViolin}}, \code{\link[ggplot2]{GeomPolygon}}
+#' @importFrom ggplot2 GeomViolin
 GeomSplitViolin <- ggplot2::ggproto("GeomSplitViolin", GeomViolin,
                            draw_group = function(self, data, ..., draw_quantiles = NULL) {
                              data <- transform(data, xminv = x - violinwidth * (x - xmin), xmaxv = x + violinwidth * (xmax - x))
